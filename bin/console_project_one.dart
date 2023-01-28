9d58c0eb4b317089f7e75991a6f892ee145c9d8c
Siddhant-ops <siddhantdalvi3@gmail.com>
2023-01-28 13:14:18 +0530

    initial commit

---
+++ bin/console_project_one.dart
@@ -0,0 +1,13 @@
+import 'package:console_project_one/console_project_one.dart'
+    as console_project_one;
+
+void main(List<String> arguments) {
+  try {
+    int args = arguments.fold(
+        0, (previousValue, element) => previousValue + int.parse(element));
+    print(
+        'Hello world: ${console_project_one.calculate()}!\nargs total is $args');
+  } catch (e) {
+    print("Error: $e");
+  }
+}
---
+++ test/console_project_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_project_one/console_project_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
